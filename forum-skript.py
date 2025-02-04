2eb1d16a3cf6ae7c5e0e4e9585ab16934dec2492
joeste054 <144039498+joeste054@users.noreply.github.com>
2025-02-04 14:22:42 +0100

    Update forum-skript.py

--- forum-skript.py
+++ forum-skript.py
@@ -4,7 +4,7 @@ import explorerhat as eh
 import qwiic_vl53l1x
 
 # Define shutdown pins
-XSHUT_pins = [1,2,3,4]  # GPIO-pins for sensor enable
+output_pins = [one,two]  # GPIO-pins for sensor enable
 
 # Define new I2C addresses
 SENSOR_ADDRESSES = [44,45]  # New addresses
@@ -14,22 +14,24 @@ def init_sensors():
     sensors = []
     
     # Set all shutdown pins to LOW to disable all sensors
-    for pin in XSHUT_pins:
-        eh.DigitalInOut(pin).direction = eh.output.off()
-        eh.DigitalInOut(pin).value = False
+    for pin in output_pins:
+        #eh.DigitalInOut(pin).direction = eh.output.off()
+        eh.output.(pin).off()
+        #eh.DigitalInOut(pin).value = False
         time.sleep(0.01)
     
     # Initialize sensors one by one
-    for i, pin in enumerate(XSHUT_pins):
-        eh.DigitalInOut(pin).value = True  # Power up sensor
+    for i, pin in enumerate(output_pins):
+        #eh.DigitalInOut(pin).value = True  # Power up sensor
+        eh.output.(pin).on()
         time.sleep(0.01)  # Wait for boot-up
-        sensor = VL53L1X(i2c)
+        #sensor = VL53L1X(i2c)
         sensor.set_address(SENSOR_ADDRESSES[i])
-        sensors.append(sensor)
+        #sensors.append(sensor)
     
     # Initialize first sensor (default address)
-    sensor1 = VL53L0X(i2c)
-    sensors.insert(0, sensor1)
+   # sensor1 = VL53L1X(i2c)
+    #sensors.insert(0, sensor1)
     
     return sensors
 
@@ -37,9 +39,9 @@ sensors = init_sensors()
 
 # Start continuous measurement
 for sensor in sensors:
-    sensor.start_continuous()
+    sensor.start_ranging()
 
 while True:
     readings = [sensor.range for sensor in sensors]
-    print(','.join(map(str, readings)))
+   # print(','.join(map(str, readings)))
     time.sleep(0.1)
