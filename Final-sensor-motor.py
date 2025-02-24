69f21f601a727795806af6d2c17bb86239dfcbfe
GA-dator <oscpre626@edu.linkoping.se>
2025-02-24 10:22:43 +0100

    2025-02-24

--- Final-sensor-motor.py
+++ Final-sensor-motor.py
@@ -8,6 +8,7 @@ import qwiic_serlcd
 print("on")
 eh.output.one.on()
 mySensor = qwiic_vl53l1x.QwiicVL53L1X()
+myLCD = qwiic_serlcd.QwiicSerlcd()     
      
 #mySensor.sensor_init()
 
@@ -24,7 +25,7 @@ except Exception as e:
 
 print("off")
 eh.output.one.off()
-
+myLCD.clearScreen()
 
 sensor1 = qwiic_vl53l1x.QwiicVL53L1X(address=0x29)
 sensor2 = qwiic_vl53l1x.QwiicVL53L1X(address=0x30)
@@ -36,16 +37,29 @@ while True:
     try:
         sensor1.start_ranging()    # Write configuration bytes to initiate measurement
         sensor2.start_ranging()    # Write configuration bytes to initiate measurement
-        time.sleep(.005)
+        time.sleep(.05)
         distance1 = sensor1.get_distance()      # Get the result of the measurement from the sensor
         distance2 = sensor2.get_distance()      # Get the result of the measurement from the sensor
-        time.sleep(.005)
+        time.sleep(.05)
         sensor1.stop_ranging()
         sensor2.stop_ranging()
-
+        
+        eh.motor.forwards(50)
+        y = str(distance1)
+        x = str(distance2)
+        
+        if distance1 < 100:
+            eh.motor.one.forwards(25)
+        
+        
+        if distance2 < 100:
+            eh.motor.two.forwards(25)
+        
+        time.sleep(.05)
+        myLCD.clearScreen()
+        myLCD.print(y + "," + x)
         
         print("Distance(mm) (1,2): (",distance1, ",",distance2, ")")
-
     except Exception as e:
         print(e)
 
