b9daced98331b3b76357b040c5d6e9a1066bde82
joeste054 <144039498+joeste054@users.noreply.github.com>
2025-04-11 16:27:13 +0200

    Update address_change_new.py

--- address_change_new.py
+++ address_change_new.py
@@ -25,6 +25,6 @@ GPIO.output(XSHUT,GPIO.HIGH)
 
 time.sleep(1)
 
-tof.start_ranging(1) # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
-distance_in_mm = tof.get_distance() # Grab the range in mm
-tof.stop_ranging() # Stop ranging
+tof.start_ranging(1) 
+distance_in_mm = tof.get_distance() 
+tof.stop_ranging() 
